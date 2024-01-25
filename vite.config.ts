5084ad9e85f117e0054a2bf734cf6d404c406f26
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-25 10:27:51 +0900

    fix: tweak optimizeDeps

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   optimizeDeps: {
     // debug by
     //   DEBUG=vite:deps pnpm test
-    force: true
+    force: true,
+    // tweak optimizeDeps for browser tests
+    // https://github.com/vitest-dev/vitest/issues/4892#issuecomment-1907879517
+    include: ["vitest > @vitest/utils > loupe"],
   },
   test: {
     browser: {
       enabled: true,
       provider: "playwright",
-      name: 'chromium',
+      name: "chromium",
     },
   },
 });

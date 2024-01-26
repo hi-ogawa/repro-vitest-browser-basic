9693dad8e4b2e31b0beb672c4ae3288d774769b9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-26 10:10:50 +0900

    chore: reproduce 4535

--- test/repro.test.ts
+++ test/repro.test.ts
@@ -2,3 +2,10 @@ import { test } from 'vitest';
 
 test('ok', async () => {
 });
+
+test("url polyfill", async () => {
+  // FAIL  test/repro.test.ts > ok
+  // ReferenceError: require is not defined
+  //  ❯ ../../../../node_modules/.pnpm/url@0.11.3/node_modules/url/url.js:26:16
+  await import("url")
+})
